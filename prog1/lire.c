dec9bdd9bed0343668ce141d5b78942e8a81bf45
monsieurclement <61271868+monsieurclement@users.noreply.github.com>
2021-12-26 19:42:37 +0100

    Update lire.c

--- prog1/lire.c
+++ prog1/lire.c
@@ -8,9 +8,11 @@
 int nb_lignes = argv[2];  */
 
 int lire(int argc, char const *argv[]) {
-    char adresse = argv[1];
-    int nb_lignes = argv[2];
-    
+    char adresse = argv[1]; // arguments
+    int nb_lignes = argv[2]; // <n>, qui va devenir un compteur
+    char lignes[nb_lignes]; //tableau pour stocker les lignes
+    int n = sizeof(lignes); // valeur fixe de <n>
+
     const char* filename = adresse; //adresse = "../Prenoms_V2/Prenoms/PrenomsV2.txt"
     
     FILE* input_file = NULL;
@@ -22,12 +24,14 @@ int lire(int argc, char const *argv[]) {
     size_t len = 0;
     while ((getline(&contents, &len, input_file) != -1) && (nb_lignes != 0)){//lecture ligne par ligne
         printf("%s", contents);
+        lignes[n - nb_lignes] = contents;
         nb_lignes -= 1;
-
     }
 
     fclose(input_file);
     free(contents);
 
-    exit(EXIT_SUCCESS);
+    return lignes;
+
+    exit(EXIT_SUCCESS);  //il veut pas se compiler je comprends paaaas
 }
